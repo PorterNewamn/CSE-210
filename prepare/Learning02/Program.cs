00fb82eb1e1b2b80899201e9366695084fbd65e5
Porter Newman <porternewman@Porters-MacBook-Air-1046.local>
2026-01-22 11:49:05 -0700

    new updated code

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        
+        Job job1 = new Job();
+        job1._jobTitle = "Webcite Developer";
+        job1._companyName = "Windowds";
+        job1._startYear = 2006;
+        job1._endyear = 2012;
+        
+
+        Job job2 = new Job();
+        job2._jobTitle = "Database";
+        job2._companyName = "Amazon";
+        job2._startYear = 2013;
+        job2._endyear = 2016;
+        
+        Resume theResume = new Resume();
+        theResume._firstNameOfImployer = "Porter";
+
+        theResume._job.Add(job1);
+        theResume._job.Add(job2);
+
+        theResume.Display();
     }
+        
+
+
+
+        
 }
